2577dc872bbea28a339f49ee946f9daa1ac3bbb7
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-12-24 10:33:41 -0800

    created bottom tabs navigation

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { View, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { Home, Restaurant, OrderDelivery } from './screens/';
+import Tabs from './navigation/Tabs';
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={'Home'}>
-        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Home' component={Tabs} />
         <Stack.Screen name='Restaurant' component={Restaurant} />
         <Stack.Screen name='OrderDelivery' component={OrderDelivery} />
       </Stack.Navigator>
