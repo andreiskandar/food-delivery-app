2432b029c898487f4d8ad4b13ee518dea216dccc
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-12-27 10:29:50 -0800

    added header

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,9 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity, FlatList, Platform } from 'react-native';
 import { COLORS, icons, SIZES, FONTS } from '../constants';
 import { initialCurrentLocation, categoryData, restaurantData } from '../mock/data';
-export default function Home() {
+export default function Home({ navigation }) {
   const [categories, setCategories] = useState(categoryData);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [restaurants, setRestaurants] = useState(restaurantData);
@@ -129,7 +130,8 @@ export default function Home() {
     const renderItem = ({ item }) => (
       <TouchableOpacity
         style={{ marginBottom: SIZES.padding * 2 }}
-        //onPress -> navigate to restaurant screen
+        // navigate to restaurant screen
+        onPress={() => navigation.navigate('Restaurant', { item, currentLocation })}
       >
         {/* Image */}
         <View style={{ marginBottom: SIZES.padding }}>
--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity, Image, Animated, Platform } from 'react-native';
+import { COLORS, icons, SIZES, FONTS } from '../constants';
 
-export default function Restaurant() {
-  return (
-    <View>
-      <Text>Restaurant</Text>
-    </View>
-  );
+export default function Restaurant({ route, navigation }) {
+  const [restaurant, setRestaurant] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  useEffect(() => {
+    let { item, currentLocation } = route.params;
+    setRestaurant(item);
+    setCurrentLocation(currentLocation);
+  });
+
+  function renderHeader() {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          style={{ width: 50, paddingLeft: SIZES.padding * 2, justifyContent: 'center' }}
+          onPress={() => navigation.goBack()}
+        >
+          <Image source={icons.back} resizeMode='contain' style={{ width: 30, height: 30 }}></Image>
+        </TouchableOpacity>
+
+        {/* Restaurant Name Section */}
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View
+            style={{
+              height: 50,
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingHorizontal: SIZES.padding * 2,
+              borderRadius: SIZES.radius,
+              backgroundColor: COLORS.lightGray3,
+            }}
+          >
+            <Text style={{ ...FONTS.h3 }}>{restaurant?.name}</Text>
+          </View>
+        </View>
+        <TouchableOpacity style={{ width: 50, paddingRight: SIZES.padding * 2, justifyContent: 'center' }}>
+          <Image source={icons.list} resizeMode='contain' style={{ width: 30, height: 30 }}></Image>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+    paddingTop: Platform.OS === 'android' ? 35 : 0,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0 },
+  },
+});
